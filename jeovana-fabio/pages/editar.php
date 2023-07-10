9241b503ae7b79194e0481f223f598b78504dca1
Jeovana Miranda <130477658+JeovanaMiranda@users.noreply.github.com>
2023-07-10 16:43:54 -0300

    Add files via upload

---
+++ jeovana-fabio/pages/editar.php
@@ -0,0 +1,32 @@
+<?php
+    $nome = $_POST['nome'];
+    $local = $_POST['city'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/editar.css">
+</head>
+<body>
+<div class="navbar">
+        <div class="logo">
+           <span>LOGO</span>
+        </div>
+        <div class="topicos">
+            <a href="index.html">
+                <p>Main</p>
+            </a>
+        </div>
+        <div class="pesquisa">
+            <div class="barraDePesquisa" >
+                <img class="iconesA"src="../img/magnifying-glass.png" alt="">
+                <input type="text" name="pesquisar" class="inputPesquisa"placeholder="Pesquisar">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
